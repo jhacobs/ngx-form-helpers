7ec517d8490c8d0a21c083189ca3b53c179b793c
Jacob Aretz <jacob.aretz@gmail.com>
2021-09-11 03:10:16 +0200

    Fix import paths

--- projects/form-helpers/src/public-api.ts
+++ projects/form-helpers/src/public-api.ts
@@ -2,14 +2,6 @@
  * Public API Surface of form-helpers
  */
 
-export * from './lib/mark-all-as-dirty';
-export * from './lib/get-all-field-names';
-export * from './lib/control-validity';
-export * from './lib/form-errors';
-export * from './lib/type.validators';
-export * from './lib/must-match.validator';
-export * from './lib/pattern-validator';
-export * from './lib/phone.validator';
-export * from './lib/address.validators';
-export * from './lib/iban.validator';
-export * from './lib/required-if.validator';
+export * from "./lib/form-helpers";
+
+export * from "./lib/validators";
